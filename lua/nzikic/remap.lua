b1e265ac63726a487a8f8df7a12c455a4b3ff9f7
Nenad Zikic <nenad.zikic@gmail.com>
2024-02-07 20:51:23 +0100

    Keymap | delete to void register

--- lua/nzikic/remap.lua
+++ lua/nzikic/remap.lua
@@ -17,3 +17,6 @@ vim.keymap.set("v", "<leader>P", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
