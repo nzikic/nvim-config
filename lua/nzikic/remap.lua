d9dc5bede56bd85044c554b60cffe4d7b914ad50
Nenad Zikic <nenad.zikic@gmail.com>
2024-02-07 20:35:21 +0100

    Keymap | Navigating through search results keeps curson in the middle of the screen

--- lua/nzikic/remap.lua
+++ lua/nzikic/remap.lua
@@ -7,3 +7,7 @@ vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- navigating through search results keeps cursor in the middle
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
