0276a9c2fd4ec1b9f07fdf48688bcc8c47df7b99
Nenad Zikic <nenad.zikic@gmail.com>
2024-02-08 01:18:37 +0100

    LSP | add select and complete keybinds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,9 +5,20 @@ lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+local cmp = require('cmp')
+cmp.setup({
+    sources = { { name = 'nvim_lsp'} },
+    mapping = {
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavcmp_select }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true })
+    }
+})
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = { 'clangd', 'cmake', 'bashls', 'tsserver', 'eslint', 'angularls' },
+	ensure_installed = { 'clangd', 'cmake', 'bashls', 'tsserver', 'eslint', 'angularls', 'lua_ls' },
 	handlers = { lsp_zero.default_setup }
 })
 
