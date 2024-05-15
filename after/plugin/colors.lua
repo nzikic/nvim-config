630625b6afbb3e90d6031c4fd4c7e70c3ec0d590
Nenad Zikic <nenad.zikic@gmail.com>
2024-05-15 23:09:43 +0200

    Default colors tokyonight

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -108,7 +108,7 @@ require("tokyonight").setup({
 
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
 --	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
