1216bf917c1eba7ee28d861f106fec4e5210fb44
Nenad Žikić <nenad.zikic@gmail.com>
2024-09-09 14:28:42 +0200

    add plugin folke/tokyonight and use it

--- lua/nzikic/plugins.lua
+++ lua/nzikic/plugins.lua
@@ -1,1 +1,11 @@
-return {} 
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+}
