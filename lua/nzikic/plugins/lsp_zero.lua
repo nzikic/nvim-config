da374bdd25d48d68f8284c44e937e2d67fa6f0b9
Nenad Žikić <nenad.zikic@gmail.com>
2024-09-10 17:03:55 +0200

    Plugin lsp-zero

---
+++ lua/nzikic/plugins/lsp_zero.lua
@@ -0,0 +1,88 @@
+return {
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
+        lazy = true,
+        config = false,
+    },
+
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'},
+        },
+        config = function()
+            local cmp = require('cmp')
+
+            cmp.setup({
+                sources = {
+                    {name = 'nvim_lsp'},
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                }),
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+            })
+        end
+    },
+
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = 'LspInfo',
+        event = {'BufReadPre', 'BufNewFile'},
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+
+            -- lsp_attach is where you enable features that only work
+            -- if there is a language server active in the file
+            local lsp_attach = function(client, bufnr)
+                local opts = {buffer = bufnr}
+
+                vim.keymap.set('n', '<leader>K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', '<leader>go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end
+
+            lsp_zero.extend_lspconfig({
+                sign_text = true,
+                lsp_attach = lsp_attach,
+                capabilities = require('cmp_nvim_lsp').default_capabilities()
+            })
+
+            require('lspconfig').clangd.setup({
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy"
+                }
+            })
+            require('lspconfig').cmake.setup({})
+            require('lspconfig').qmlls.setup({})
+            require('lspconfig').bashls.setup({})
+            require('lspconfig').ts_ls.setup({})
+            require('lspconfig').eslint.setup({})
+            require('lspconfig').lua_ls.setup({})
+            require('lspconfig').yamlls.setup({})
+            require('lspconfig').jsonls.setup({})
+        end
+    }
+}
