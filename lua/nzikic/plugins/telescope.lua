4d60a2cfc2f733fbd86f92e55972168a69824775
Nenad Žikić <nenad.zikic@gmail.com>
2024-09-10 14:15:11 +0200

    Confugre telescope in lazy config

--- lua/nzikic/plugins/telescope.lua
+++ lua/nzikic/plugins/telescope.lua
@@ -1,5 +1,30 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Find files in source control" })
+
+
+        -- Clone the default telescope configuration
+        local telescopeConfig = require('telescope.config')
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        table.insert(vimgrep_arguments, "--hidden") -- search in dotfiles
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*") -- dont search '.git' dir
+
+        require('telescope').setup({
+            defaults = {
+                vimgrep_arguments = vimgrep_arguments,
+                preview = { filesize = 1 --[[ MB --]] },
+                pickers = {},
+            },
+        })
+    end
 }
