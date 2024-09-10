8c9960ac9ef3833429a74637c791a6c73aadf250
Nenad Žikić <nenad.zikic@gmail.com>
2024-09-10 11:59:32 +0200

    plugin treesitter

--- lua/nzikic/plugins.lua
+++ lua/nzikic/plugins.lua
@@ -8,4 +8,18 @@ return {
             vim.cmd([[colorscheme tokyonight]])
         end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = { "c", "cpp", "cmake", "bash", "vim", "vimdoc", "lua" },
+                auto_install = true,
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
 }
