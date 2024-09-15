6c5664f7964c977e680d965af960d7660725c241
Nenad Zikic <nenad.zikic@gmail.com>
2024-09-15 14:18:11 +0200

    lualine uses 'nightfly' instead of 'tokyonight'

--- lua/nzikic/plugins/lualine.lua
+++ lua/nzikic/plugins/lualine.lua
@@ -6,8 +6,8 @@ return {
         lualine.setup({
             options = {
                 icons_enabled = true,
-                --theme = 'nightfly',
-                theme = 'tokyonight',
+                theme = 'nightfly',
+                --theme = 'tokyonight',
                 component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
                 disabled_filetypes = {
