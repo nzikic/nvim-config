cfef9277d73d821846933bec7f40b168dc19c141
Nenad Zikic <nenad.zikic@gmail.com>
2024-11-07 21:44:14 +0100

    Treesitter | use specific compilers if on Windows

--- lua/nzikic/plugins/treesitter.lua
+++ lua/nzikic/plugins/treesitter.lua
@@ -2,6 +2,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+        if jit and jit.os == "Windows" then
+            local install = require("nvim-treesitter.install")
+            install.compilers = { "cl", "zig", "clang", "gcc" }
+        end
+
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             ensure_installed = { "c", "cpp", "cmake", "bash", "vim", "vimdoc", "lua", "javascript" },
