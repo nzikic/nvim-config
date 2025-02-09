d0dce26c5821f09626c5d3a0571b003792e9b939
Nenad Zikic <nenad.zikic@gmail.com>
2025-02-09 17:26:32 +0100

    Lua LSP | add lazydev.nvim plugin so Lua LSP knows about vim global variable

--- lua/nzikic/plugins/lsp_zero.lua
+++ lua/nzikic/plugins/lsp_zero.lua
@@ -59,6 +59,17 @@ return {
             {'hrsh7th/cmp-nvim-lsp'},
             {'williamboman/mason.nvim'},
             {'williamboman/mason-lspconfig.nvim'},
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
         },
         config = function()
             local lsp_zero = require('lsp-zero')
