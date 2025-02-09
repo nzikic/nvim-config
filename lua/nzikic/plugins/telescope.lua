f0af12577779cfddcb48a5cc5ba83196c887729a
Nenad Zikic <nenad.zikic@gmail.com>
2025-02-09 17:26:53 +0100

    Telescope | Support grepping specific file types

--- lua/nzikic/plugins/telescope.lua
+++ lua/nzikic/plugins/telescope.lua
@@ -1,3 +1,52 @@
+local generate_rg_command = function(prompt)
+    if not prompt or prompt == "" then
+        return nil
+    end
+
+    local args = { "rg" }
+
+    local delimiter_2_spaces = "  "
+    local pieces = vim.split(prompt, delimiter_2_spaces)
+
+    if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+    end
+
+    if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+    end
+
+    return vim.tbl_flatten {
+        args,
+        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
+    }
+end
+
+local custom_multi_grep = function(opts)
+    opts = opts or {}
+    opts.cwd = opts.cwd or vim.uv.cwd()
+
+    local make_entry = require("telescope.make_entry")
+    local finders = require("telescope.finders")
+    local finder = finders.new_async_job({
+        command_generator = generate_rg_command,
+        entry_maker = make_entry.gen_from_vimgrep(opts),
+        cwd = opts.cwd,
+    })
+
+    local conf = require("telescope.config").values
+    local pickers = require("telescope.pickers")
+    pickers.new(opts, {
+        prompt_title = "Multi Grep",
+        finder = finder,
+        debounce = 100,
+        previewer = conf.grep_previewer(opts),
+        sorter = require("telescope.sorters").empty()
+    }):find()
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
@@ -8,7 +57,8 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fg', custom_multi_grep)
         vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
