0d50f59e19ad76f30df510b0063f6b0340d5ac50
Nenad Zikic <nenad.zikic@gmail.com>
2025-03-10 11:11:00 +0100

    Configure copilot.lua

--- lua/nzikic/plugins/completion.lua
+++ lua/nzikic/plugins/completion.lua
@@ -1,7 +1,10 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = {
+    { 'rafamadriz/friendly-snippets' },
+    { 'giuxtaposition/blink-cmp-copilot' },
+  },
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -38,7 +41,15 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
 
     -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
