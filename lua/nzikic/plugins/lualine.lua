8e86d18031a7e0885ed685fea8063046fddeb14b
Nenad Zikic <nenad.zikic@gmail.com>
2025-03-18 19:46:40 +0100

    Lualine | filename section before git info section

--- lua/nzikic/plugins/lualine.lua
+++ lua/nzikic/plugins/lualine.lua
@@ -25,16 +25,16 @@ return {
             },
             sections = {
                 lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_b = {'filename'},
+                lualine_c = {'branch', 'diff', 'diagnostics'},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
             },
             inactive_sections = {
                 lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename'},
+                lualine_b = {'filename'},
+                lualine_c = {},
                 lualine_x = {'location'},
                 lualine_y = {},
                 lualine_z = {}
