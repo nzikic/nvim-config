b5c3ff20c762b980de0e8e890c3fc02b2d3e3b62
Nenad Zikic <nenad.zikic@gmail.com>
2025-04-04 21:04:01 +0200

    add render markdown plugin

---
+++ lua/nzikic/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown', 'copilot-chat' },
+    opts = {
+        filetypes = { 'markdown', 'copilot-chat' },
+        completions = { lsp = { enabled = true } }
+    },
+}
