a7e3fa6bee26f0c1b6271734afa60278fcd760b2
Nenad Zikic <nenad.zikic@gmail.com>
2025-07-30 00:37:16 +0200

    Install DAP plugins with minimal basic config

---
+++ lua/nzikic/plugins/dap.lua
@@ -0,0 +1,44 @@
+local setup_adapters = function(dap)
+  dap.adapters.gdb = {
+    type = "executable",
+    command = "gdb",
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+  }
+
+end
+
+local M = {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
+    "williamboman/mason.nvim",
+  },
+  config = function ()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    setup_adapters(dap)
+  end
+}
+
+return M
