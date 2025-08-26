668d48a023ba6224b8fcef2e0793b273934a3860
Nenad Zikic <nenad.zikic@gmail.com>
2025-08-26 14:35:14 +0200

    log highlight now higlights log and LOG extensions for unix systems

--- lua/nzikic/plugins/log-highlight.lua
+++ lua/nzikic/plugins/log-highlight.lua
@@ -1,6 +1,9 @@
 local M = {
-    'fei6409/log-highlight.nvim',
-    opts = {},
+  'fei6409/log-highlight.nvim',
+  opts = {},
+  config = function()
+    require('log-highlight').setup { extension = { 'log', 'LOG' } }
+  end
 }
 
 return M
