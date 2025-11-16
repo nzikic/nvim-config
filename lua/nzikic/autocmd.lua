e50652fd5cf02593db201a25dea69896bad10014
Nenad Zikic <nenad.zikic@gmail.com>
2025-11-16 19:17:39 +0100

    Format autocmd.lua

--- lua/nzikic/autocmd.lua
+++ lua/nzikic/autocmd.lua
@@ -1,41 +1,41 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = "Highlight when yanking text",
-    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-    callback = function ()
-        vim.highlight.on_yank()
-    end,
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'Lsp actions',
-    callback = function (event)
-        local opts = { buffer = event.buf }
+  desc = 'Lsp actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
 
-        vim.keymap.set('n', '<leader>K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', '<leader>go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x', 'v' }, '<F3>', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-        vim.keymap.set('n', '<F5>', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    vim.keymap.set('n', '<leader>K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', '<leader>go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x', 'v' }, '<F3>', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set('n', '<F5>', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
 
-        vim.keymap.set('i', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    end
+    vim.keymap.set('i', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  end
 })
 
 vim.api.nvim_create_autocmd('BufEnter', {
-    desc = 'Copilot chat: set relative numbers, and treat copilot chat buffer filetype as markdown',
-    pattern = 'copilot-*',
-    callback = function ()
-        vim.opt_local.relativenumber = true
-        vim.opt_local.number = true
-        local ft = vim.bo.filetype
-        if ft == 'copilot-chat' then
-            vim.bo.filetype = 'markdown'
-        end
+  desc = 'Copilot chat: set relative numbers, and treat copilot chat buffer filetype as markdown',
+  pattern = 'copilot-*',
+  callback = function()
+    vim.opt_local.relativenumber = true
+    vim.opt_local.number = true
+    local ft = vim.bo.filetype
+    if ft == 'copilot-chat' then
+      vim.bo.filetype = 'markdown'
     end
+  end
 })
