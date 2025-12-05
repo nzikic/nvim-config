8a14095fce31e0fca217739d8de95a2cdcd46f63
Nenad Zikic <nenad.zikic@gmail.com>
2025-12-05 12:13:31 +0100

    CopilotChat | Define custom system prompt for coding assistant instructions

--- lua/nzikic/plugins/copilot-chat.lua
+++ lua/nzikic/plugins/copilot-chat.lua
@@ -1,16 +1,27 @@
 local M = {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  version = "v4.7.4",
-  opts = {
-    model = "claude-sonnet-4.5",
-    mappings = {
-      close = {
-        normal = 'q',
-        insert = ''
+  'CopilotC-Nvim/CopilotChat.nvim',
+  version = 'v4.7.4',
+  opts = function ()
+    local predef_instructions = require('CopilotChat.config.prompts').COPILOT_INSTRUCTIONS.system_prompt
+    local x = require('CopilotChat.config.prompts').Commit
+
+    return {
+      model = 'claude-sonnet-4.5',
+      mappings = {
+        close = {
+          normal = 'q',
+          insert = ''
+        },
       },
-    },
-  },
-  event = "VeryLazy",
+      prompts = {
+        coding_assistant = {
+          system_prompt = predef_instructions .. 'Do things step by step and in small increments. Before answering always analyze the problem and code thoroughly. When answering, always layout analysis and plan of action concisely and in steps. Do implementation only when explicitly asked.',
+          description = 'Coding assistant instructions',
+        },
+      },
+    }
+  end,
+  event = 'VeryLazy',
   keys = {
     { '<leader>at', '<cmd>CopilotChatToggle<CR>', desc = 'CopilotChat | Toggle chat' },
     { '<leader>as', '<cmd>CopilotChatStop<CR>',   desc = 'CopilotChat | Stop current output' },
